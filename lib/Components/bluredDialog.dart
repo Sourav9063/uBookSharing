d5ce24ff7388d2b32f995be59ef32949fd5c0b50
Sourav9063 <53114581+Sourav9063@users.noreply.github.com>
2021-01-15 00:21:14 +0600

    Update blur radious

--- lib/Components/bluredDialog.dart
+++ lib/Components/bluredDialog.dart
@@ -18,19 +18,20 @@ class BlurredDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Dialog(
-
       insetPadding: EdgeInsets.symmetric(horizontal: 0, vertical: 0),
       clipBehavior: Clip.antiAlias,
       backgroundColor: Colors.transparent,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
       elevation: 0,
       child: BackdropFilter(
         filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
         child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            color: blurColorWithOpacity,
-            // border: Border.all(color: Colors.white, width: 2)
-          ),
+          color: blurColorWithOpacity,
+          // decoration: BoxDecoration(
+          //   borderRadius: BorderRadius.circular(16),
+          //   color: blurColorWithOpacity,
+          //   border: Border.all(color: Colors.white, width: 2)
+          // ),
           height: height,
           width: width,
           child: Padding(padding: const EdgeInsets.all(8.0), child: child),
